346c54518caec47896d573f1b9d224c847dcf01b
petirrozora <fathir.ilham2405@gmail.com>
2025-11-10 20:33:44 +0700

    Update UI folder kontak dan layout

---
+++ uas-framework/resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', config('app.name', 'Laravel'))</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="font-sans antialiased bg-gray-50 text-gray-900">
+
+    {{-- Navbar --}}
+    @include('partials.navbar')
+
+    {{-- Page Content --}}
+    <main class="min-h-screen pt-24">
+        @yield('content')
+    </main>
+
+    {{-- Footer --}}
+    @include('partials.footer')
+
+</body>
+</html>
